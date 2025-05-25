b147969f3ecb8c1d3e7d197da7d8deb5aa7be161
Georgi Koychev <georgi.koychev@accedia.com>
2025-05-25 03:40:57 +0300

    add screenshots upon fial

--- environment.py
+++ environment.py
@@ -15,6 +15,24 @@ def before_scenario(context, scenario):
         options=options
     )
 
+def after_step(context, step):
+    if step.status == "failed":
+        os.makedirs("screenshots", exist_ok=True)
+
+        screenshot_name = f"screenshot_{step.name}.png".replace(" ", "_").replace("/", "_")
+        screenshot_path = os.path.join("screenshots", screenshot_name)
+
+        try:
+            context.driver.save_screenshot(screenshot_path)
+            with open(screenshot_path, "rb") as image_file:
+                allure.attach(
+                    image_file.read(),
+                    name=step.name,
+                    attachment_type=AttachmentType.PNG
+                )
+        except Exception as e:
+            print(f"Неуспешно заснемане на screenshot: {e}")
+
 def after_scenario(context, scenario):
     if context.driver:
         context.driver.quit()
